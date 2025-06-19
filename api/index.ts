31f9d616321524998d41a611bbc0307c683a5289
azamalidev <aazam7246@gmail.com>
2025-06-19 16:06:39 +0500

    New Change

--- api/index.ts
+++ api/index.ts
@@ -2,8 +2,16 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello from Express & TypeScript deployed on Vercel!</h1>');
+app.get('/', (req: any, res: any) => {
+  res.send('<h1>Hello World from Express & TypeScript on Vercel!</h1>');
 });
+
+// // ✅ Listen on a port locally, skip it on Vercel
+// if (process.env.NODE_ENV !== 'production') {
+//   const PORT = process.env.PORT || 3000;
+//   app.listen(PORT, () => {
+//     console.log(`Running locally → http://localhost:${PORT}`);
+//   });
+// }
 
 export default app;
