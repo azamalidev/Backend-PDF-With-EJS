50ab4dfd2f4422ab12a97a5c0162ddcb27adc04e
azamalidev <aazam7246@gmail.com>
2025-06-19 20:23:48 +0500

    New Setup

---
+++ api/index.ts
@@ -0,0 +1,6 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
