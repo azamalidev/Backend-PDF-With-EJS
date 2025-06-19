6e9dedebe67add01112da9414cb4bfb924e34bfc
azamalidev <aazam7246@gmail.com>
2025-06-19 21:14:17 +0500

    New Setup

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,3 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (_req: any, res: { send: (arg0: string) => any; }) => res.send("Express on Vercel"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
+export default function handler(req:any, res:any) {
+  res.status(200).send("Hello from Vercel API 🚀");
+}
